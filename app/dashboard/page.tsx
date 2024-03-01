e978ed3eff58711de9dba3f7e7137c95ea346340
Priyanshu Singh <reveur.guy99@gmail.com>
2024-03-02 01:04:06 +0530

    try to fix error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { addData } from "@/lib/supabase/helpers"
 import { createClient } from "@/lib/supabase/server"
 import { redirect } from "next/navigation"
 
+export const dynamic = "force-dynamic"
+
 export default async function DashboardPage() {
   const supabase = createClient()
   const { data, error } = await supabase.auth.getUser()
