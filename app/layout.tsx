611f9aec97db6fe05702b3969d19befd3bc16ad5
Priyanshu Singh <reveur.guy99@gmail.com>
2024-03-01 20:30:28 +0530

    removed provider from layout, removed api/auth file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import { Toaster } from "@/components/ui/toaster"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import SupabaseProvider from "./supabase-provider"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -19,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SupabaseProvider>{children}</SupabaseProvider>
+        {children}
         <Toaster />
       </body>
     </html>
