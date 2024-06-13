0bb55089028e1eca5438bf21a146d18f02708791
Priyanshu Singh <reveur.guy99@gmail.com>
2024-06-13 20:38:03 +0530

    added api to get user details

---
+++ app/api/user/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ data: data.user });
+}
